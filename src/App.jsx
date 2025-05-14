b1c84cd6232072ddd8baac3de940b973e4f7619f
Tamerlan <tamerlan.musaev@mail.ru~>
2025-05-15 01:29:18 +0300

    changes Tommy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import Catalog from "./pages/catalog/catalog";
-import PageMain from "./pages/main/pageMain";
-import "./scss/app.scss";
-import Basket from "./components/basket/basket";
-import CatalogById from "./components/catalogById/catalogById";
+import { useSelector } from 'react-redux'
+import { Route, Routes } from "react-router-dom"
+import Basket from "./components/basket/basket"
+import Card from './components/catalog/cards'
+import CatalogById from "./components/catalogById/catalogById"
+import Catalog from "./pages/catalog/catalog"
+import PageMain from "./pages/main/pageMain"
+import "./scss/app.scss"
 
 function App() {
+
+
+  const { items, status } = useSelector((state) => state.product);
+
+  if(!items) return <h1>Loading...</h1>
+
   return (
     <div className="wrapper">
       <Routes>
-        <Route path="/catalog" element={<Catalog />} />
+        {/* <Route path="/catalog" element={<Catalog />} /> */}
+
+
+        <Route path="/catalog" element={<Catalog />}>
+          <Route path="" element={items.map((item) => <Card key={item.id} {...item} />)} /> 
+          <Route path=":id" element={<Card />} /> 
+        </Route>
+
+
         <Route path="/" element={<PageMain />} />
         <Route path="/basket" element={<Basket />} />
         <Route path="/catalogById/:id" element={<CatalogById />} />
