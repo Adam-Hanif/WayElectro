be78e63189b2e540d35f390f5d2fe61856a14fe8
Adam <Alan_Salons@mail.ru>
2025-08-23 10:32:49 +0300

    init

--- src/features/catalog/model/slices/circuitBreakersSlice.js
+++ src/features/catalog/model/slices/circuitBreakersSlice.js
@@ -1,60 +1,79 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const fetchCircuitBreakers = createAsyncThunk(
-	'CircuitBreakers/fetchBrand',
-	async ({ id }) => {
-		const { data } = await axios.get(
-			`https://way-electro-server.onrender.com/get_circuit_breakers_list/${id}`
-		);
-		return data;
-	}
+  "CircuitBreakers/fetchBrand",
+  async ({ id }) => {
+    const { data } = await axios.get(
+      `https://way-electro-server.onrender.com/get_circuit_breakers_list/${id}`
+    );
+    return data;
+  }
 );
 
 const initialState = {
-	itemCircuitBreakers: [],
+  itemCircuitBreakers: [],
   allItems: [],
-	statusCircuitBreakers: 'loading',
+  statusCircuitBreakers: "loading",
+  filter: {
+    brand: "all",
+    poles: "all",
+    text: "",
+  },
 };
 
 const circuitBreakersSlice = createSlice({
-	name: 'CircuitBreakers',
-	initialState,
-	reducers: {
-		setItems(state, action) {
-			state.itemCircuitBreakers = action.payload;
-		},
-		getItemsById(state, action) {
-			if (action.payload === 'all') {
-				state.itemCircuitBreakers = state.allItems;
-			} else {
-				state.itemCircuitBreakers = state.allItems.filter(obj => {
-					console.log(obj.brand_id, action.payload);
+  name: "CircuitBreakers",
+  initialState,
+  reducers: {
+    setItems(state, action) {
+      state.itemCircuitBreakers = action.payload;
+    },
+    clearFilter(state) {
+      state.filter = { brand: "all", poles: "all", text: "" };
+      state.itemCircuitBreakers = state.allItems;
+    },
+    setFilter(state, action) {
+      // Обновляем фильтр
+      state.filter = { ...state.filter, ...action.payload };
 
-					return obj.brand_id === action.payload;
-				});
-			}
-		},
-	},
+      // Фильтруем товары
+      state.itemCircuitBreakers = state.allItems.filter((item) => {
+        const brandMatch =
+          state.filter.brand === "all" ||
+          String(item.brand_id) === String(state.filter.brand);
 
-	extraReducers: builder => {
-		builder.addCase(fetchCircuitBreakers.pending, state => {
-			state.statusCircuitBreakers = 'loading';
-			state.itemCircuitBreakers = [];
+        const polesMatch =
+          state.filter.poles === "all" ||
+          Number(item.number_of_poles) === Number(state.filter.poles);
+        const textMatch =
+          !state.filter.text ||
+          item.name.toLowerCase().includes(state.filter.text.toLowerCase());
+
+        return brandMatch && polesMatch && textMatch;
+      });
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCircuitBreakers.pending, (state) => {
+      state.statusCircuitBreakers = "loading";
+      state.itemCircuitBreakers = [];
       state.allItems = [];
-		});
-		builder.addCase(fetchCircuitBreakers.fulfilled, (state, action) => {
-			state.itemCircuitBreakers = action.payload;
+    });
+    builder.addCase(fetchCircuitBreakers.fulfilled, (state, action) => {
+      state.itemCircuitBreakers = action.payload;
       state.allItems = action.payload;
-			state.statusCircuitBreakers = 'resolved';
-		});
-		builder.addCase(fetchCircuitBreakers.rejected, (state, action) => {
-			state.statusCircuitBreakers = 'error';
-			state.error = action.payload;
-			state.itemCircuitBreakers = [];
+      state.statusCircuitBreakers = "resolved";
+    });
+    builder.addCase(fetchCircuitBreakers.rejected, (state, action) => {
+      state.statusCircuitBreakers = "error";
+      state.error = action.payload;
+      state.itemCircuitBreakers = [];
       state.allItems = [];
-		});
-	},
+    });
+  },
 });
-export const { setItems, getItemsById } = circuitBreakersSlice.actions;
+
+export const { setItems, setFilter, clearFilter } =
+  circuitBreakersSlice.actions;
 export default circuitBreakersSlice.reducer;
--- src/features/catalog/ui/searchInpute/index.jsx
+++ src/features/catalog/ui/searchInpute/index.jsx
@@ -1,84 +1,101 @@
-import exitIcon from '@shared/assets/images/exitIcon.svg';
-import debounce from 'lodash.debounce';
-import React from 'react';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { getItemsById } from '../../model/slices/circuitBreakersSlice';
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import debounce from "lodash.debounce";
+import exitIcon from "@shared/assets/images/exitIcon.svg";
+import {
+  fetchCircuitBreakers,
+  setFilter,
+  clearFilter,
+} from "../../model/slices/circuitBreakersSlice";
 
-// import { setSearchValue } from "../../redux/slices/filterSlice";
 function SearchInpute() {
-	const [value, setValue] = React.useState('');
-	const dispatch = useDispatch();
-	const inputRef = React.useRef();
-	const { itemBrand, statusBrand } = useSelector(
-		state => state.catalogReducer.cartBrandSlice
-	);
+  const dispatch = useDispatch();
+  const inputRef = useRef();
+  const [searchValue, setSearchValue] = useState("");
 
-	const options = itemBrand.map(item => ({
-		value: String(item.id),
-		label: item.Name,
-	}));
+  const { allItems, filter } = useSelector(
+    (state) => state.catalogReducer.circuitBreakersSlice
+  );
+  const { itemBrand } = useSelector(
+    (state) => state.catalogReducer.cartBrandSlice
+  );
+  // Загружаем все товары при монтировании
+  useEffect(() => {
+    dispatch(fetchCircuitBreakers());
+  }, [dispatch]);
 
-	const options2 = ['1', '2', '3', '4'];
+  const optionsBrand = [
+    { value: "all", label: "Все бренды" },
+    ...itemBrand.map((item) => ({ value: item.id, label: item.Name })),
+  ];
+  const optionsPoles = [
+    { value: "all", label: "Все" },
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+  ];
 
-	const onClckClear = () => {
-		setValue('');
-		setSearchValue('');
-		inputRef.current.focus();
-	};
+  // Очистка поиска
+  const clearSearch = () => {
+    setSearchValue("");
+    inputRef.current.focus();
+    dispatch(clearFilter());
+  };
 
-	const updaraSearchValue = React.useCallback(
-		debounce(str => {
-			dispatch(setSearchValue(str));
-		}, 250),
-		[]
-	);
+  // Дебаунс для текстового поиска
+  const debouncedSearch = useCallback(
+    debounce((value) => {
+      dispatch(setFilter({ text: value }));
+    }, 300),
+    []
+  );
 
-	const onChangeInput = event => {
-		setValue(event.target.value);
-		updaraSearchValue(event.target.value);
-	};
-	return (
-		<div className='block_search'>
-			<div className='search_filter'>
-				<Dropdown
-					onChange={e => {
-						dispatch(getItemsById(e.value));
-					}}
-					options={options}
-					placeholder='Бренды'
-				/>
-			</div>
-			<div className='search_filter'>
-				<Dropdown
-					onChange={e => {
-						console.log(e.value);
-					}}
-					options={options2}
-					placeholder='Количество полюсов'
-				/>
-			</div>
-			<div className='search_input'>
-				<input
-					ref={inputRef}
-					value={value}
-					onChange={onChangeInput}
-					className='search'
-					type='text'
-					placeholder='Поиск по каталогу'
-				/>
-				{value && (
-					<img
-						onClick={onClckClear}
-						className='exitIcon'
-						src={exitIcon}
-						alt=''
-					/>
-				)}
-			</div>
-		</div>
-	);
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+    debouncedSearch(e.target.value);
+  };
+
+  return (
+    <div className="block_search">
+      <div className="search_filter">
+        <Dropdown
+          options={optionsBrand}
+          placeholder="Бренды"
+          onChange={(e) => dispatch(setFilter({ brand: e.value }))}
+        />
+      </div>
+
+      <div className="search_filter">
+        <Dropdown
+          options={optionsPoles}
+          placeholder="Количество полюсов"
+          onChange={(e) => dispatch(setFilter({ poles: e.value }))}
+        />
+      </div>
+
+      <div className="search_input">
+        <input
+          ref={inputRef}
+          type="text"
+          value={searchValue}
+          onChange={handleInputChange}
+          className="search"
+          placeholder="Поиск по каталогу"
+        />
+        {searchValue && (
+          <img
+            className="exitIcon"
+            src={exitIcon}
+            alt="clear"
+            onClick={clearSearch}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default SearchInpute;
