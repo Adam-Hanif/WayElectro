5cbb0b2f7e30d9dbe738632844e366d0d4bbea90
Adam <Alan_Salons@mail.ru>
2025-09-10 10:26:14 +0300

    init

--- src/features/catalog/ui/catalogLayout/ProductCard.jsx
+++ src/features/catalog/ui/catalogLayout/ProductCard.jsx
@@ -15,43 +15,27 @@ function ProductCard({ id, image_src, name, unit = "шт", price }) {
   );
 
   const existingItem = cartItems.find((item) => item.id === id);
-  const initialCount = existingItem ? existingItem.count : 0;
-
-  const [count, setLocalCount] = useState(initialCount);
-  const [isBuying, setIsBuying] = useState(initialCount > 0);
+  const count = existingItem ? existingItem.count : 0;
+  const isBuying = count > 0;
 
   const handleBuyClick = (e) => {
     e.preventDefault();
     if (!isBuying) {
-      setIsBuying(true);
-      setLocalCount(1);
       dispatch(addToCart({ id, name, image_src, unit }));
     }
   };
 
   const handleIncrease = () => {
-    setLocalCount((prev) => prev + 1);
     dispatch(increaseCount(id));
   };
 
   const handleDecrease = () => {
-    if (count > 1) {
-      setLocalCount((prev) => prev - 1);
-      dispatch(decreaseCount(id));
-    } else {
-      setLocalCount(0);
-      setIsBuying(false);
-      dispatch(decreaseCount(id)); // удалит товар из корзины
-    }
+    dispatch(decreaseCount(id));
   };
 
   const handleInputChange = (e) => {
     let value = parseInt(e.target.value, 10);
     if (isNaN(value) || value < 0) value = 0;
-
-    setLocalCount(value);
-    setIsBuying(value > 0);
-
     dispatch(setCount({ id, count: value }));
   };
 
@@ -66,22 +50,25 @@ function ProductCard({ id, image_src, name, unit = "шт", price }) {
 
       {!isBuying ? (
         <div className="btn_item">
-          <button className="btn_produktCard btn_details">Подробности</button>
+          <div>Артикуль {}</div>
           <button className="btn_produktCard" onClick={handleBuyClick}>
             Купить
           </button>
         </div>
       ) : (
-        <div className="counter">
-          <button onClick={handleDecrease}>-</button>
-          <input
-            type="number"
-            min="0"
-            value={count}
-            onChange={handleInputChange}
-          />
-          <span>{unit}</span>
-          <button onClick={handleIncrease}>+</button>
+        <div className="btn_item">
+          <div>Артикуль {}</div>
+          <div className="counter">
+            <button onClick={handleDecrease}>-</button>
+            <input
+              type="number"
+              min="0"
+              value={count}
+              onChange={handleInputChange}
+            />
+            <span>{unit}</span>
+            <button onClick={handleIncrease}>+</button>
+          </div>
         </div>
       )}
     </div>
